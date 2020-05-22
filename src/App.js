36ed52640b3fdfa284248779af1d0d9c8ed250f7
Omar Dakkak <omardakkak98@gmail.com>
2020-05-22 21:53:13 +0000

    update

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Users from './users/pages/Users';
 import NewModule from './modules/pages/NewModule';
 import UserModules from './modules/pages/UserModules';
 import UpdateModule from './modules/pages/UpdateModule';
+import Auth from './users/pages/Auth';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
 
 const App = () => {
@@ -25,6 +26,9 @@ const App = () => {
           </Route>
           <Route path="/Modules/:moduleId">
             <UpdateModule/>
+          </Route>
+          <Route path="/auth">
+            <Auth/>
           </Route>
           <Redirect to="/"/>
         </Switch>
